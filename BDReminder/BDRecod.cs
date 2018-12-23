507942598bfbccebb3c1ad7806792135ccb62b8d
Alex <iamwebdev@gmail.com>
2018-12-22 23:11:47 -0500

    Add project files.

---
+++ BDReminder/BDRecod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BDReminder
+{
+    public class BDRecod
+    {
+        int id;
+        string name;
+        DateTime date;
+
+    }
+}
